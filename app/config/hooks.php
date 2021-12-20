38009c01fd1554b9e06579abacb68e2182341492
giorgosstab <tsaxre1991@hotmail.com>
2021-12-21 00:16:36 +0200

    Fix hook file autoload hooks and aliases to facades

--- app/config/hooks.php
+++ app/config/hooks.php
@@ -3,7 +3,23 @@
 use Elegant\Foundation\Hooks\Hook;
 
 $hook = Hook::autoload([
-    'whoops' => Elegant\Foundation\Hooks\Whoops::class,
-    'route' => Elegant\Foundation\Hooks\Routing::class,
-    'view' => Elegant\Foundation\Hooks\View::class
+    'providers' => [
+
+        /*
+         * Laraigniter Framework Service Providers...
+         */
+        Elegant\Routing\RouteServiceProvider::class,
+        Elegant\Foundation\Exceptions\WhoopsServiceProvider::class,
+        Elegant\View\ViewServiceProvider::class,
+
+
+        /*
+         * Application Service Providers...
+         */
+        App\Providers\ViewServiceProvider::class
+    ],
+
+    'aliases' => [
+        'blade' => Elegant\Support\Facades\Blade::class,
+    ]
 ]);
